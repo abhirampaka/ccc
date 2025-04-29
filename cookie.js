9d2c18d062fd06036da54e69a9acdfee458e01ce
abhirampaka <abhirampaka18@outlook.com>
2025-04-29 10:21:31 +0530

    "working on cookie example"

--- cookie.js
+++ cookie.js
@@ -1,21 +1,38 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var app = express();
+
 app.use(cookieParser());
-app.get('/my_cookieset',(req,res)=>{
-    res.cookie('Name','Nikhil').send('Cookie is set');
+
+// Set cookie
+app.get('/my_cookieset', (req, res) => {
+    res.cookie('Name', 'Nikhil');
+    res.cookie('Age', '89');
+    res.status(200).send("Cookie is set")
+
 });
-app.get('/',(req,res)=>{
-    console.log("Cookies:",req.cookies);
-});
-app.get('/my_cookieget',(req,res)=>{
-    res.status(200).send(req.cookies);
-});
-app.get('/',(req,res)=>{
+
+// Get cookies
+app.get('/', (req, res) => {
+    console.log("Cookies:", req.cookies);
     res.status(200).send("It is a cookie");
 });
-app.get('/my_cookieclear',(req,res)=>{
+
+// Get cookies in API
+app.get('/my_cookieget', (req, res) => {
+    res.status(200).send(req.cookies);
+});
+
+// Clear cookie
+app.get('/my_cookieclear', (req, res) => {
+    res.clearCookie('Name');
     res.clearCookie('Age');
     res.status(200).send("Cookie is removed");
 });
 
+// Start server
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
