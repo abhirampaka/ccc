1509a0e97f8696a5a33cb2027a741bdc5d42d1e4
abhirampaka <abhirampaka18@outlook.com>
2025-04-29 11:02:11 +0530

    adding new files

---
+++ getexample1.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+app.get('/get.html',(req,res)=>{
+    res.sendFile(__dirname + '/'+'get.html');
+});
+app.get('/process_get',(req,res)=>{
+    response = {
+        first_name : req.query.first_name,
+        last_name : req.query.last_name
+    };
+    console.log(response);
+    res.end("In Web Page = " + JSON.stringify(response));
+})
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+});
