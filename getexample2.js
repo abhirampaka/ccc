af8e3dacb12b64d37a91599e3f5fb782e220f723
abhirampaka <abhirampaka18@outlook.com>
2025-04-29 11:58:20 +0530

    added all changes

--- getexample2.js
+++ getexample2.js
@@ -5,15 +5,19 @@ app.get('/get.html',(req,res)=>{
     res.sendFile(__dirname + '/'+'get1.html');
 });
 app.get('/process_get', (req, res) => {
-    response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name,
-        gender: req.query.gender,
-        password: req.query.password,
-        about: req.query.about
-    };
-    console.log(response);
-    res.end("In Web Page = " + JSON.stringify(response));
+    // response = {
+    //     first_name: req.query.first_name,
+    //     last_name: req.query.last_name,
+    //     gender: req.query.gender,
+    //     password: req.query.password,
+    //     about: req.query.about
+    // };
+    // console.log(response);
+    res.send('<p>FirstName: '+req.query['first_name']+'</p>'+'<p>LastName: '+req.query['last_name']+
+        '</p>'+'<p>Gender: '+req.query['gender']+'</p>'+'<p>Password: '+req.query['password']+'</p>'+
+        '<p>About: '+req.query['about']+'</p>'
+    );
+    // res.end("In Web Page = " + JSON.stringify(response));
 });
 
 var server = app.listen(8080, function() {
